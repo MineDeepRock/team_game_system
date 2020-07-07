f788e8c9e62c1642525464b5e94f84ecc3d497ca
suinua <maatinosippo@gmail.com>
2020-07-07 23:01:10 +0900

    getterの追加

--- src/team_game_system/model/Map.php
+++ src/team_game_system/model/Map.php
@@ -7,11 +7,31 @@ namespace team_game_system\model;
 class Map
 {
 
+    /**
+     * @var string
+     */
     private $name;
+    /**
+     * @var SpawnPoint[]
+     */
     private $spawnPoints;
 
     public function __construct(string $name, array $spawnPoints) {
         $this->name = $name;
         $this->spawnPoints = $spawnPoints;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string {
+        return $this->name;
+    }
+
+    /**
+     * @return SpawnPoint[]
+     */
+    public function getSpawnPoints(): array {
+        return $this->spawnPoints;
     }
 }
--- src/team_game_system/model/SpawnPoint.php
+++ src/team_game_system/model/SpawnPoint.php
@@ -8,11 +8,31 @@ use pocketmine\math\Vector3;
 
 class SpawnPoint
 {
+    /**
+     * @var TeamId
+     */
     private $teamId;
+    /**
+     * @var Vector3
+     */
     private $position;
 
     public function __construct(TeamId $teamId, Vector3 $position) {
         $this->teamId = $teamId;
         $this->position = $position;
+    }
+
+    /**
+     * @return TeamId
+     */
+    public function getTeamId(): TeamId {
+        return $this->teamId;
+    }
+
+    /**
+     * @return Vector3
+     */
+    public function getPosition(): Vector3 {
+        return $this->position;
     }
 }
