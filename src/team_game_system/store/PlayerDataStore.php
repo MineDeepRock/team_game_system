11cdf5fcf654588669494f62f5ae4edc39e511af
suinua <maatinosippo@gmail.com>
2020-07-07 19:25:46 +0900

    PlayerDataStoreを実装

--- src/team_game_system/store/PlayerDataStore.php
+++ src/team_game_system/store/PlayerDataStore.php
@@ -4,7 +4,37 @@
 namespace team_game_system\store;
 
 
+use team_game_system\data_model\PlayerData;
+
 class PlayerDataStore
 {
+    /**
+     * @var PlayerData[]
+     */
+    static $playersData;
 
+    static function add(PlayerData $playerData): void {
+        self::$playersData[] = $playerData;
+    }
+
+    static function findByName(string $name): PlayerData {
+        $results = array_filter(self::$playersData, function ($playerData) use ($name) {
+            return $playerData->getName() === $name;
+        });
+
+        return $results[0];
+    }
+
+    static function remove(string $name): void {
+        $results = array_filter(self::$playersData, function ($playerData) use ($name) {
+            return $playerData->getName() !== $name;
+        });
+
+        self::$playersData = $results;
+    }
+
+    static function update(PlayerData $playerData): void {
+        self::remove($playerData->getName());
+        self::add($playerData);
+    }
 }
