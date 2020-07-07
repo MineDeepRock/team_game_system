4ffa263d3408ed9fb51f488635fc0008aa0d7ec0
suinua <maatinosippo@gmail.com>
2020-07-07 20:25:10 +0900

    joinGameの実装

--- src/team_game_system/TeamGameSystem.php
+++ src/team_game_system/TeamGameSystem.php
@@ -4,9 +4,14 @@
 namespace team_game_system;
 
 
+use pocketmine\Player;
 use team_game_system\model\Game;
 use team_game_system\model\GameId;
+use team_game_system\model\TeamId;
 use team_game_system\service\CreateGameService;
+use team_game_system\service\JoinGameService;
+use team_game_system\service\StartGameService;
+use team_game_system\store\GameStore;
 
 class TeamGameSystem
 {
@@ -16,5 +21,14 @@ class TeamGameSystem
 
     static function startGame(GameId $gameId): void {
         StartGameService::execute($gameId);
+    }
+
+    static function joinGame(Player $player, GameId $gameId, ?TeamId $teamId): void {
+        JoinGameService::execute($player, $gameId, $teamId);
+        $game = GameStore::findById($gameId);
+
+        if ($game->isStarted()) {
+            //TODO : 途中参加
+        }
     }
 }
--- src/team_game_system/service/JoinGameService.php
+++ src/team_game_system/service/JoinGameService.php
@@ -7,11 +7,18 @@ namespace team_game_system\service;
 use team_game_system\data_model\PlayerData;
 use team_game_system\model\GameId;
 use team_game_system\model\TeamId;
+use team_game_system\store\GameStore;
 use team_game_system\store\PlayerDataStore;
 
 class JoinGameService
 {
-    static function execute(string $name, GameId $gameId, TeamId $teamId): void {
-        PlayerDataStore::update(new PlayerData($name, $gameId, $teamId));
+    static function execute(string $name, GameId $gameId, ?TeamId $teamId): void {
+        if ($teamId === null) {
+            $game = GameStore::findById($gameId);
+            $teamId = SortTeamsByPlayersService::execute($game->getTeams())[0];
+            PlayerDataStore::update(new PlayerData($name, $gameId, $teamId));
+        } else {
+            PlayerDataStore::update(new PlayerData($name, $gameId, $teamId));
+        }
     }
 }
