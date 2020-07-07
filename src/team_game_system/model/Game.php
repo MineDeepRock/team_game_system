786f2664bc32ae16c20b4721b62302ea3fab8706
suinua <maatinosippo@gmail.com>
2020-07-07 19:25:07 +0900

    update

--- src/team_game_system/model/Game.php
+++ src/team_game_system/model/Game.php
@@ -5,6 +5,55 @@ namespace team_game_system\model;
 
 class Game
 {
+    /**
+     * @var GameId
+     */
     private $id;
+    /**
+     * @var Map
+     */
+    private $map;
+    /**
+     * @var Team[]
+     */
     private $teams;
+
+    private $isStarted;
+
+    public function __construct(GameId $id,Map $map,array $teams) {
+        $this->id = $id;
+        $this->map = $map;
+        $this->teams = $teams;
+
+        $this->isStarted = false;
+    }
+
+    static function asNew(Map $map, array $teams): Game {
+        return new Game(GameId::asNew(), $map, $teams);
+    }
+
+    /**
+     * @return GameId
+     */
+    public function getId(): GameId {
+        return $this->id;
+    }
+
+    /**
+     * @return Map
+     */
+    public function getMap(): Map {
+        return $this->map;
+    }
+
+    /**
+     * @return Team[]
+     */
+    public function getTeams(): array {
+        return $this->teams;
+    }
+
+    public function start(): void {
+        $this->isStarted = true;
+    }
 }
