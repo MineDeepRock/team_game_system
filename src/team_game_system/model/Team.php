51983985af4cf1530edc824a029412ba3dbc7b14
suinua <maatinosippo@gmail.com>
2020-07-07 20:24:40 +0900

    teamがscoreを持つように

--- src/team_game_system/model/Team.php
+++ src/team_game_system/model/Team.php
@@ -7,17 +7,45 @@ use pocketmine\utils\Color;
 
 class Team
 {
+    /**
+     * @var TeamId
+     */
     private $id;
+    /**
+     * @var string
+     */
     private $name;
+    /**
+     * @var Color
+     */
     private $teamColor;
+    /**
+     * @var Score
+     */
+    private $score;
 
-    public function __construct(TeamId $id, string $name, Color $teamColor) {
+    public function __construct(TeamId $id, string $name, Color $teamColor,Score $score) {
         $this->id = $id;
         $this->name = $name;
         $this->teamColor = $teamColor;
+        $this->score = $score;
     }
 
     static function asNew(string $name, Color $teamColor): Team {
-        return new Team(TeamId::asNew(), $name, $teamColor);
+        return new Team(TeamId::asNew(), $name, $teamColor,Score::asNew());
+    }
+
+    /**
+     * @return Score
+     */
+    public function getScore(): Score {
+        return $this->score;
+    }
+
+    /**
+     * @return TeamId
+     */
+    public function getId(): TeamId {
+        return $this->id;
     }
 }
