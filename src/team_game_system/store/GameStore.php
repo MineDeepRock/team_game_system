6eac951788d07b75034b1331c855a8b4c5b44226
suinua <maatinosippo@gmail.com>
2020-07-07 19:25:33 +0900

    add `findById` method

--- src/team_game_system/store/GameStore.php
+++ src/team_game_system/store/GameStore.php
@@ -4,12 +4,24 @@ namespace team_game_system\store;
 
 
 use team_game_system\model\Game;
+use team_game_system\model\GameId;
 
 class GameStore
 {
+    /**
+     * @var Game[]
+     */
     static private $games;
 
     static function add(Game $game): void {
         $game[] = $game;
+    }
+
+    static function findById(GameId $gameId): Game {
+        $results = array_filter(self::$games, function ($game) use ($gameId) {
+            return $game->getId()->equals($gameId);
+        });
+
+        return $results[0];
     }
 }
