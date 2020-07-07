d42c79aa4eabb940263b33bd4fa8a68e3842c08d
suinua <maatinosippo@gmail.com>
2020-07-07 19:24:58 +0900

    PlayerDataが参加してるゲームIDを所持するように

--- src/team_game_system/data_model/PlayerData.php
+++ src/team_game_system/data_model/PlayerData.php
@@ -3,13 +3,39 @@
 namespace team_game_system\data_model;
 
 
+use team_game_system\model\GameId;
+use team_game_system\model\TeamId;
+
 class PlayerData
 {
     private $name;
+    private $gameId;
     private $teamId;
 
-    public function __construct($name, $teamId) {
+    public function __construct(string $name, GameId $gameId, TeamId $teamId) {
         $this->name = $name;
         $this->teamId = $teamId;
+        $this->gameId = $gameId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string {
+        return $this->name;
+    }
+
+    /**
+     * @return TeamId
+     */
+    public function getTeamId(): TeamId {
+        return $this->teamId;
+    }
+
+    /**
+     * @return GameId
+     */
+    public function getGameId(): GameId {
+        return $this->gameId;
     }
 }
