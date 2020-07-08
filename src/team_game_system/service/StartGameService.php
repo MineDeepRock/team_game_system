66f389bd2609bb2252062f35d714048c999bec2a
suinua <maatinosippo@gmail.com>
2020-07-08 16:55:07 +0900

    StartGameServiceの実装

--- src/team_game_system/service/StartGameService.php
+++ src/team_game_system/service/StartGameService.php
@@ -4,10 +4,11 @@ namespace team_game_system\service;
 
 
 use team_game_system\model\GameId;
+use team_game_system\store\GameStore;
 
 class StartGameService
 {
     static function execute(GameId $gameId):void {
-
+        GameStore::findById($gameId)->start();
     }
 }
