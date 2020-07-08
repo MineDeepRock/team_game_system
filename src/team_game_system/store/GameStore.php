437f30e9eef45190b3294ac49c2919145364aa16
suinua <maatinosippo@gmail.com>
2020-07-08 21:30:56 +0900

    add delete and getALl

--- src/team_game_system/store/GameStore.php
+++ src/team_game_system/store/GameStore.php
@@ -17,11 +17,22 @@ class GameStore
         $game[] = $game;
     }
 
+    static function delete(GameId $gameId): void {
+
+        foreach (self::$games as $game) {
+            if($game->getId()->equals($gameId)) unset($game);
+        }
+    }
+
     static function findById(GameId $gameId): Game {
         $results = array_filter(self::$games, function ($game) use ($gameId) {
             return $game->getId()->equals($gameId);
         });
 
         return $results[0];
+    }
+
+    static function getAll(): array {
+        return self::$games;
     }
 }
