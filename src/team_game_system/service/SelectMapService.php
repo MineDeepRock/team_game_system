f59c92737c51223e4b7501d4987646f6d087dc84
suinua <maatinosippo@gmail.com>
2020-07-08 19:39:24 +0900

    SelectMapServiceの追加

---
+++ src/team_game_system/service/SelectMapService.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace team_game_system\service;
+
+
+use team_game_system\adapter\MapJsonAdapter;
+use team_game_system\model\Map;
+
+class SelectMapService
+{
+    const PATH = "./plugin_data/TeamGameSystem/";
+
+    static function byName(string $name): Map {
+        $mapsData = json_decode(file_get_contents(self::PATH . $name . ".json"), true);
+        return MapJsonAdapter::decode($mapsData);
+    }
+
+    static function random(): Map {
+        $maps = [];
+        $dh = opendir(self::PATH);
+        while (($fileName = readdir($dh)) !== false) {
+            if (filetype(self::PATH . $fileName) === "file") {
+                $data = json_decode(file_get_contents(self::PATH . $fileName), true);
+                $maps[] = MapJsonAdapter::decode($data);
+            }
+        }
+
+        closedir($dh);
+
+        return array_rand($maps);
+    }
+}
