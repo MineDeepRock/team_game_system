b8e064906ed4ff9b2c3d791938c3768faf7717ef
suinua <maatinosippo@gmail.com>
2020-07-08 16:56:10 +0900

    StartGameEventの追加

---
+++ src/team_game_system/pmmp/event/StartGameEvent.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace team_game_system\pmmp\event;
+
+
+use pocketmine\event\Event;
+use team_game_system\model\GameId;
+
+class StartGameEvent extends Event
+{
+    /**
+     * @var GameId
+     */
+    private $gameId;
+
+    public function __construct(GameId $gameId) {
+        $this->gameId = $gameId;
+    }
+
+    /**
+     * @return GameId
+     */
+    public function getGameId(): GameId {
+        return $this->gameId;
+    }
+}
