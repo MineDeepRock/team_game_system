9a023a8982edab90c7ab15eb2e8fafceab9c0981
suinua <maatinosippo@gmail.com>
2020-07-08 16:56:36 +0900

    ゲームスタート時にStartGameEventを呼ぶように

--- src/team_game_system/pmmp/service/StartGamePMMPService.php
+++ src/team_game_system/pmmp/service/StartGamePMMPService.php
@@ -5,11 +5,14 @@ namespace team_game_system\pmmp\service;
 
 use pocketmine\Server;
 use team_game_system\model\GameId;
+use team_game_system\pmmp\event\StartGameEvent;
 use team_game_system\store\PlayerDataStore;
 
 class StartGamePMMPService
 {
     static function execute(GameId $gameId): void {
+        $event = new StartGameEvent($gameId);
+        $event->call();
         $players = PlayerDataStore::getGamePlayers($gameId);
         foreach ($players as $player) {
             SetSpawnPMMPService::execute(Server::getInstance()->getPlayer($player->getName()));
