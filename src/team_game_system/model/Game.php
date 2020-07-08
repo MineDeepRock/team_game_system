e8e29867b58bd0033065b3ff11399bd3e4b74923
suinua <maatinosippo@gmail.com>
2020-07-08 21:31:18 +0900

    maxScore,maxPlayersCount,timeLimit

--- src/team_game_system/model/Game.php
+++ src/team_game_system/model/Game.php
@@ -17,19 +17,37 @@ class Game
      * @var Team[]
      */
     private $teams;
+    /**
+     * @var Score
+     */
+    private $maxScore;
+    /**
+     * @var int
+     */
+    private $maxPlayersCount;
+    /**
+     * @var int
+     * second
+     */
+    private $timeLimit;
 
     private $isStarted;
+    private $isClosed;
 
-    public function __construct(GameId $id,Map $map,array $teams) {
+    public function __construct(GameId $id, Map $map, array $teams, ?Score $maxScore = null, ?int $maxPlayersCount = null, ?int $timeLimit = null) {
         $this->id = $id;
         $this->map = $map;
         $this->teams = $teams;
 
         $this->isStarted = false;
+        $this->isClosed = false;
+        $this->maxScore = $maxScore;
+        $this->maxPlayersCount = $maxPlayersCount;
+        $this->timeLimit = $timeLimit;
     }
 
-    static function asNew(Map $map, array $teams): Game {
-        return new Game(GameId::asNew(), $map, $teams);
+    static function asNew(Map $map, array $teams, ?Score $maxScore = null, ?int $maxPlayersCount = null, ?int $timeLimit = null): Game {
+        return new Game(GameId::asNew(), $map, $teams, $maxScore, $maxPlayersCount, $timeLimit);
     }
 
     /**
@@ -57,10 +75,21 @@ class Game
         $this->isStarted = true;
     }
 
+    public function close(): void {
+        $this->isClosed = true;
+    }
+
     /**
      * @return bool
      */
     public function isStarted(): bool {
         return $this->isStarted;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isClosed(): bool {
+        return $this->isClosed;
     }
 }
