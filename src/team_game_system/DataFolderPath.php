86a129d12005f0e5c246a04a37cab6530f18e7fe
suinua <maatinosippo@gmail.com>
2020-07-09 21:59:31 +0900

    DataFolderPathからpathを参照するように

---
+++ src/team_game_system/DataFolderPath.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace team_game_system;
+
+
+class DataFolderPath
+{
+    const PATH = "./plugin_data/TeamGameSystem/";
+    const MAP = self::PATH . "maps/";
+
+}
--- src/team_game_system/service/SelectMapService.php
+++ src/team_game_system/service/SelectMapService.php
@@ -5,14 +5,13 @@ namespace team_game_system\service;
 
 
 use team_game_system\adapter\MapJsonAdapter;
+use team_game_system\DataFolderPath;
 use team_game_system\model\Map;
 
 class SelectMapService
 {
-    const PATH = "./plugin_data/TeamGameSystem/";
-
     static function byName(string $name): Map {
-        $mapsData = json_decode(file_get_contents(self::PATH . $name . ".json"), true);
+        $mapsData = json_decode(file_get_contents(DataFolderPath::MAP . $name . ".json"), true);
         return MapJsonAdapter::decode($mapsData);
     }
 
@@ -24,10 +23,10 @@ class SelectMapService
 
     static function all(): array {
         $maps = [];
-        $dh = opendir(self::PATH);
+        $dh = opendir(DataFolderPath::MAP);
         while (($fileName = readdir($dh)) !== false) {
-            if (filetype(self::PATH . $fileName) === "file") {
-                $data = json_decode(file_get_contents(self::PATH . $fileName), true);
+            if (filetype(DataFolderPath::MAP . $fileName) === "file") {
+                $data = json_decode(file_get_contents(DataFolderPath::MAP . $fileName), true);
                 $maps[] = MapJsonAdapter::decode($data);
             }
         }
