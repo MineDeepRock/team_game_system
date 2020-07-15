45f1a6440e8d220215e54fffe0a68d70976db654
suinua <maatinosippo@gmail.com>
2020-07-15 20:32:44 +0900

    PlayerQuitGameEventが抜けたプレイヤーが入っていたチームIDを持つように

--- src/team_game_system/pmmp/event/PlayerQuitGameEvent.php
+++ src/team_game_system/pmmp/event/PlayerQuitGameEvent.php
@@ -7,6 +7,7 @@ namespace team_game_system\pmmp\event;
 use pocketmine\event\Event;
 use pocketmine\Player;
 use team_game_system\model\GameId;
+use team_game_system\model\TeamId;
 
 class PlayerQuitGameEvent extends Event
 {
@@ -14,15 +15,19 @@ class PlayerQuitGameEvent extends Event
      * @var Player
      */
     private $player;
-
     /**
      * @var GameId
      */
     private $gameId;
+    /**
+     * @var TeamId
+     */
+    private $teamId;
 
-    public function __construct(Player $player, GameId $gameId) {
+    public function __construct(Player $player, GameId $gameId,TeamId $teamId) {
         $this->player = $player;
         $this->gameId = $gameId;
+        $this->teamId = $teamId;
     }
 
     /**
@@ -37,5 +42,12 @@ class PlayerQuitGameEvent extends Event
      */
     public function getGameId(): GameId {
         return $this->gameId;
+    }
+
+    /**
+     * @return TeamId
+     */
+    public function getTeamId(): TeamId {
+        return $this->teamId;
     }
 }
--- src/team_game_system/pmmp/service/QuitGamePMMPService.php
+++ src/team_game_system/pmmp/service/QuitGamePMMPService.php
@@ -6,12 +6,13 @@ namespace team_game_system\pmmp\service;
 
 use pocketmine\Player;
 use team_game_system\model\GameId;
+use team_game_system\model\TeamId;
 use team_game_system\pmmp\event\PlayerQuitGameEvent;
 
 class QuitGamePMMPService
 {
-    static function execute(Player $player, GameId $gameId) {
-        $event = new PlayerQuitGameEvent($player, $gameId);
+    static function execute(Player $player, GameId $gameId, TeamId $teamId) {
+        $event = new PlayerQuitGameEvent($player, $gameId, $teamId);
         $event->call();
     }
 }
