49dff0690ceca7b87bfeddf023123cace9973688
suinua <maatinosippo@gmail.com>
2020-07-16 21:26:51 +0900

    StartGamePMMPServiceからSetSpawnPMMPServiceを呼び出さないように
    代わりにユーザーが呼び出す

--- src/team_game_system/pmmp/service/StartGamePMMPService.php
+++ src/team_game_system/pmmp/service/StartGamePMMPService.php
@@ -3,19 +3,13 @@
 namespace team_game_system\pmmp\service;
 
 
-use pocketmine\Server;
 use team_game_system\model\GameId;
 use team_game_system\pmmp\event\StartedGameEvent;
-use team_game_system\store\PlayerDataStore;
 
 class StartGamePMMPService
 {
     static function execute(GameId $gameId): void {
         $event = new StartedGameEvent($gameId);
         $event->call();
-        $players = PlayerDataStore::getGamePlayers($gameId);
-        foreach ($players as $player) {
-            SetSpawnPMMPService::execute(Server::getInstance()->getPlayer($player->getName()));
-        }
     }
 }
