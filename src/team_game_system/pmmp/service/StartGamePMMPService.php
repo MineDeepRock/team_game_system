fe5c991ca849a8d681ff0c5225e854733881fb6f
suinua <maatinosippo@gmail.com>
2020-08-03 21:23:33 +0900

    ゲーム開始前に選択したマップのLevelを読み込むように

--- src/team_game_system/pmmp/service/StartGamePMMPService.php
+++ src/team_game_system/pmmp/service/StartGamePMMPService.php
@@ -3,12 +3,17 @@
 namespace team_game_system\pmmp\service;
 
 
+use pocketmine\Server;
 use team_game_system\model\GameId;
 use team_game_system\pmmp\event\StartedGameEvent;
+use team_game_system\store\GameStore;
 
 class StartGamePMMPService
 {
     static function execute(GameId $gameId): void {
+        $map = GameStore::findById($gameId)->getMap();
+        Server::getInstance()->loadLevel($map->getLevelName());
+
         $event = new StartedGameEvent($gameId);
         $event->call();
     }
