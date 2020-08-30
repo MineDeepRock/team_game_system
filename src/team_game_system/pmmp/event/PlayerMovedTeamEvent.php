8c0d23dabb59438fcdbf3a520b68a1431725ae10
suinua <maatinosippo@gmail.com>
2020-08-31 00:04:58 +0900

    add PlayerMovedTeamEvent

---
+++ src/team_game_system/pmmp/event/PlayerMovedTeamEvent.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace team_game_system\pmmp\event;
+
+
+use pocketmine\event\Event;
+use pocketmine\Player;
+use team_game_system\model\GameId;
+
+class PlayerMovedTeamEvent extends Event
+{
+    /**
+     * @var Player
+     */
+    private $player;
+
+    /**
+     * @var GameId
+     */
+    private $gameId;
+
+    public function __construct(Player $player, GameId $gameId) {
+        $this->player = $player;
+        $this->gameId = $gameId;
+    }
+
+    /**
+     * @return Player
+     */
+    public function getPlayer(): Player {
+        return $this->player;
+    }
+
+    /**
+     * @return GameId
+     */
+    public function getGameId(): GameId {
+        return $this->gameId;
+    }
+}
